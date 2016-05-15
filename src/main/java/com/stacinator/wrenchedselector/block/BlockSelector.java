42f14aa498471761346e4b3d17830961538c94df
ThomasRules2000 <tstacey2000@gmail.com>
2016-05-15 21:52:42 +0100

    Fixed class reference and added basis for selection

--- src/main/java/com/stacinator/wrenchedselector/block/BlockSelector.java
+++ src/main/java/com/stacinator/wrenchedselector/block/BlockSelector.java
@@ -1,5 +1,12 @@
 package com.stacinator.wrenchedselector.block;
 
+import cpw.mods.fml.common.registry.GameData;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentText;
+
+import java.util.Random;
+import java.util.Set;
+
 public class BlockSelector extends BlockWS
 {
     public BlockSelector()
@@ -7,5 +14,41 @@ public class BlockSelector extends BlockWS
         super();
         this.setBlockName("selector");
         this.setBlockTextureName("selector");
+
+        boolean[] item = {false,false,false,false};
+        int[] randomNum = new int[4];
+
+        Set blocks = GameData.getBlockRegistry().getKeys();
+        Set items = GameData.getItemRegistry().getKeys();
+
+        int blockLength = blocks.size();
+        int itemLength = items.size();
+        int total = blockLength + itemLength;
+
+        for(int i = 0; i>3;)
+        {
+            Random rand = new Random();
+            randomNum[i] = rand.nextInt(total-1)+1;
+
+            boolean same = false;
+            for(int j = 0; j>i; j++)
+            {
+                if(randomNum[i] == randomNum[j])
+                {
+                    same = true;
+                }
+            }
+
+            if(!same)
+            {
+                i++;
+            }
+
+            if(randomNum[i] > blockLength)
+            {
+                randomNum[i] -= blockLength;
+                item[i] = true;
+            }
+        }
     }
 }
--- src/main/java/com/stacinator/wrenchedselector/init/ModBlocks.java
+++ src/main/java/com/stacinator/wrenchedselector/init/ModBlocks.java
@@ -1,5 +1,6 @@
 package com.stacinator.wrenchedselector.init;
 
+import com.stacinator.wrenchedselector.block.BlockSelector;
 import com.stacinator.wrenchedselector.block.BlockWS;
 import com.stacinator.wrenchedselector.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -7,7 +8,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks
 {
-    public static final BlockWS selector = new BlockWS();
+    public static final BlockWS selector = new BlockSelector();
 
     public static void init()
     {
